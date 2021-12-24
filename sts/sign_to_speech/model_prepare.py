eff964b6ea582b2b5331a27604575661f14177e2
azizmousa <azizmousa1010@gmail.com>
2021-12-24 20:53:25 +0200

    add download_model docstring"

--- sts/sign_to_speech/model_prepare.py
+++ sts/sign_to_speech/model_prepare.py
@@ -1,9 +1,24 @@
+import os
 import requests
 
 
 def download_model(model_url):
-    request = requests.get(model_url, allow_redirects=True)
-    open('cv_model.h5', 'wb').write(request.content)
+    """
+    def download_model(model_url)
+    download pretrained h5 model file
+    Args:
+        model_url (str): model download url
+
+    Returns: True if download succeed
+            False otherwise
+    """
+    try:
+        request = requests.get(model_url, allow_redirects=True)
+        os.makedirs('model', exist_ok=True)
+        open(os.path.join('model', 'cv_model.h5'), 'wb').write(request.content)
+        return True
+    except:
+        return False
 
 
 def update_model(model_path):
