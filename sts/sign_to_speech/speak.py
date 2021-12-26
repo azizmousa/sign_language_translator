6652b2952a4b15ea1961e3a49740db915ee99212
azizmousa <azizmousa1010@gmail.com>
2021-12-27 01:17:11 +0200

    document the speak class

--- sts/sign_to_speech/speak.py
+++ sts/sign_to_speech/speak.py
@@ -4,13 +4,26 @@ from playsound import playsound
 
 
 class Speak:
-
+    """
+    Speak class convert the input text to voice
+    """
     def __init__(self, language='en', accent='com', slow=False):
         self.__language = language
         self.__accent = accent
         self.__slow = slow
 
     def speak(self, text):
+        """
+        def speak(self, text)
+
+        function to convert the input text voice
+
+        Args:
+            text (str): string text input
+
+        Returns: None
+
+        """
         if text != "":
             output = gtts.gTTS(text=text, lang=self.__language, slow=self.__slow, tld=self.__accent)
             output.save("speak.mp3")
