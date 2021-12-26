ecd0cc0af592f0906ca8d88d81d78dd55fb8547d
azizmousa <azizmousa1010@gmail.com>
2021-12-27 01:14:55 +0200

    document the parser class

--- sts/sign_to_speech/parser.py
+++ sts/sign_to_speech/parser.py
@@ -3,12 +3,25 @@ from happytransformer import TTSettings
 
 
 class Parser:
-
+    """
+    Parser class convert some words to a right english statement
+    """
     def __init__(self):
         self.__happy_tt = HappyTextToText("T5",  "prithivida/grammar_error_correcter_v1")
         self.__settings = TTSettings(do_sample=True, top_k=10, temperature=0.5, min_length=1, max_length=100)
 
     def parse(self, text):
+        """
+        def parse(self, text)
+
+        convert the input text to right english statement
+
+        Args:
+            text (str): string of some english words
+
+        Returns: dictionary of the right english statement
+
+        """
         tmp_text = "gec: " + text
         result = self.__happy_tt.generate_text(tmp_text, args=self.__settings)
         return result.text
