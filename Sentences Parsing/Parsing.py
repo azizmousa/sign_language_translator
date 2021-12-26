25db0d775c7c82e03b78b29b3bd6d3582cf5a53b
Amira Mosa <45495863+AmiraMosa@users.noreply.github.com>
2021-12-26 17:51:26 +0200

    Add files via upload

---
+++ Sentences Parsing/Parsing.py
@@ -0,0 +1,12 @@
+from happytransformer import HappyTextToText
+from happytransformer import TTSettings
+
+
+happy_tt = HappyTextToText("T5",  "vennify/t5-base-grammar-correction")
+settings = TTSettings(do_sample=True, top_k=10, temperature=0.5,  min_length=1, max_length=100)
+
+
+def parsing(text):
+    tmp_text = "gec: " + text
+    result = happy_tt.generate_text(text, args=settings)
+    return result.text
