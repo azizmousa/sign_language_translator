eda0e9735a1540b977ace4069a586fc270180c3d
azizmousa <azizmousa1010@gmail.com>
2021-12-27 01:11:12 +0200

    document the model class

--- sts/sign_to_speech/model.py
+++ sts/sign_to_speech/model.py
@@ -1,4 +1,3 @@
-import os
 import numpy as np
 import tensorflow as tf
 import mediapipe as mp
@@ -6,6 +5,17 @@ from cv2 import cv2
 
 
 def read_labels(labels_path):
+    """
+    def read_labels(labels_path)
+
+    function to read labels from names file
+
+    Args:
+        labels_path (str): names file path
+
+    Returns: dictionary of labels, id pairs.
+
+    """
     with open(labels_path, 'r') as names_file:
         lines = names_file.read()
     i = 0
@@ -17,6 +27,10 @@ def read_labels(labels_path):
 
 
 class Model:
+    """
+    Model class is a class to start continuous stream from the input source and classify the motions of
+    sign language to words
+    """
 
     def __init__(self, stream_source, sequence_length, model_path, labels_path,
                  display_keypoint=False, display_window=True):
@@ -37,12 +51,37 @@ class Model:
         self.display_window = display_window
 
     def detect_keypoints(self, image):
+        """
+        def detect_keypoints(self, image):
+
+        detect the keypoints from an input image
+
+        Args:
+            image (2d-np-array): the input image
+
+        Returns:
+            image: the input image,
+            results: the keypoints results
+
+        """
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         results = self.holistic.process(image)
         image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
         return image, results
 
     def draw_landmarks(self, image, results):
+        """
+        def draw_landmarks(self, image, results)
+
+        function to draw the landmarks on the input image
+
+        Args:
+            image: input image that you want to draw the landmarks on
+            results: the landmarks that you want to draw
+
+        Returns: None
+
+        """
         self.mp_drawing.draw_landmarks(image, results.face_landmarks, self.mp_holistic_model.FACEMESH_CONTOURS,
                                        self.mp_drawing.DrawingSpec(color=(10, 194, 80), thickness=1, circle_radius=1),
                                        self.mp_drawing.DrawingSpec(color=(214, 200, 80), thickness=1, circle_radius=1))
@@ -57,6 +96,17 @@ class Model:
                                        self.mp_drawing.DrawingSpec(color=(190, 200, 80), thickness=2, circle_radius=4))
 
     def extract_keypoints(self, results):
+        """
+        def extract_keypoints(self, results)
+
+        function to extrack the keypoints from the landmarks
+
+        Args:
+            results: object that contains the keypoints
+
+        Returns: np-array that contain all keypoints sequentially
+
+        """
         face_landmarks = np.array([[lm.x, lm.y, lm.z] for lm in
                                    results.face_landmarks.landmark]).flatten() \
             if results.face_landmarks else np.zeros(468 * 3)
@@ -72,7 +122,14 @@ class Model:
         return np.concatenate([pose_landmarks, face_landmarks, left_hand_landmarks, right_hand_landmarks])
 
     def start_stream(self):
+        """
+        def start_stream(self)
 
+        function to start the source input and predict the sign language from the source
+
+        Returns: generator of the predicted words
+
+        """
         sequence = []
         sentence = []
         predictions = []
