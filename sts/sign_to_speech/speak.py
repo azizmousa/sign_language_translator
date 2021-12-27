9bac3e0fa6b42baf730d22329822e8eca251a81b
azizmousa <azizmousa1010@gmail.com>
2021-12-27 02:17:42 +0200

    document speak class

--- sts/sign_to_speech/speak.py
+++ sts/sign_to_speech/speak.py
@@ -6,6 +6,19 @@ from playsound import playsound
 class Speak:
     """
     Speak class convert the input text to voice
+
+    Attributes:
+        __language (string, optional): The language (IETF language tag) to
+            read the text in. Default is ``en``.
+
+        __accent (string, optional): Top-level domain for the Google Translate host,
+            i.e `https://translate.google.<tld>`. Different Google domains
+            can produce different localized 'accents' for a given
+            language. This is also useful when ``google.com`` might be blocked
+            within a network but a local or different Google host
+            (e.g. ``google.cn``) is not. Default is ``com``.
+
+        __slow (bool, optional): Reads text more slowly. Defaults to ``False``.
     """
     def __init__(self, language='en', accent='com', slow=False):
         self.__language = language
@@ -14,8 +27,6 @@ class Speak:
 
     def speak(self, text):
         """
-        def speak(self, text)
-
         function to convert the input text voice
 
         Args:
