f62ead64b65ca7a544e438e31d909833d352f6de
azizmousa <azizmousa1010@gmail.com>
2021-12-29 23:07:20 +0200

    python package files

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+ 
+classifiers = [
+  'Intended Audience :: Education',
+  'Operating System :: OS Independent',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3.8',
+  'Topic :: Sign Language :: ASL'
+]
+
+
+with open("README.md", "r") as readme:
+    long_descript = readme.read()
+ 
+
+setup(
+  name = 'sts',
+  version = '1.0.0',
+  
+  description = 'Converting the American sign language into speech or text, and vice versa.',
+  long_description = long_descript,
+  long_description_content_type = "text/markdown",
+  
+  url = 'https://github.com/azizmousa/sign_language_translator',  
+  author = 'azizmousa',
+  author_email = 'azizmousa1010@gmail.com',
+  
+  license = 'MIT', 
+  classifiers = classifiers,
+  keywords = 'ASL translation', 
+  packages = find_packages(),
+  install_requires = ['requirements.txt'] 
+)
